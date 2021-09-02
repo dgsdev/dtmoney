55f2cfd72baf8918874f2f2d07e60cf0a6351dc1
Douglas Pinheiro <dgspramos@gmail.com>
2021-09-01 23:35:09 -0300

    Intro de styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css';
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #0099cc;
+`
 
 export function App() {
   return (
-    <div className="App">     
-        <h1 className="title">Hello New World React Typescript 2021</h1>     
+    <div className="App">
+      <Title>Hello New World Typescript 2021</Title>
     </div>
   );
 }
-
-

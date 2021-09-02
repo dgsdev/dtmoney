c40bc8a60845a6ad98ce11ad97b46b86508019c0
Douglas Pinheiro <dgspramos@gmail.com>
2021-09-01 23:20:44 -0300

    annotation e css exemplo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import './App.css';
+
 export function App() {
   return (
     <div className="App">     
-        <h1>Hello New World React Typescript 2021</h1>     
+        <h1 className="title">Hello New World React Typescript 2021</h1>     
     </div>
   );
 }

afbf6d4e6fa7f032079ff80d43c2fa364d87f1ff
Douglas Pinheiro <dgspramos@gmail.com>
2021-09-01 23:38:31 -0300

    Adicionado favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #0099cc;
-`
-
 export function App() {
   return (
     <div className="App">
-      <Title>Hello New World Typescript 2021</Title>
+      <h1>Hello New World Typescript 2021</h1>
     </div>
   );
 }

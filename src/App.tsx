a540310a70dcafd535a23ae21374c89d74e4d53d
Douglas Pinheiro <dgspramos@gmail.com>
2021-09-02 00:00:16 -0300

    Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello New World Typescript 2021</h1>
+      <GlobalStyle />
     </div>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+      --background: #F0F2F5;
+      --red: #e52e4d;
+      --blue: #5429cc;
+      --blue-light: #6933ff;
+      --text-title: #363f5f;
+      --text-body: #969cb3;
+      --shape: #ffffff;
+  }
+
+  * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+  }
+
+  // font-size: 16px (Desktop)
+  html {
+      @media (max-width: 1080px) {
+      font-size: 93.75%; //15px
+      }
+      @media (max-width: 720px){
+          font-size: 87.5%; //14px
+      }
+  }
+
+  //REM = 1rem = font-size = 16px
+  //Percentual --> Adapta para vários tipos de telas
+
+  body {
+      background-color: var(---background);
+      -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+      cursor: pointer;
+  }
+
+  [disabled] {
+      opacity: 0.6;
+      cursor: not-allowed;
+  }
+  
+  `

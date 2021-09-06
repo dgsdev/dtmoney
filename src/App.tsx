7d7c8b40e3ddbfc14788c10383353a397d8e5b36
Douglas Pinheiro <dgspramos@gmail.com>
2021-09-06 16:41:17 -0300

    Componente NewTransactionModal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement("#root");
@@ -22,13 +23,12 @@ export function App() {
 
       <Dashboard />
 
-      <Modal 
-            isOpen={isNewTransactionModalOpen} 
-            onRequestClose={handleCloseNewTransactionModal}
-            >
-                <h1>Cadastrar Nova Transação</h1>
-            </Modal>   
-
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+       
+       />
+      
       <GlobalStyle />
     </>
   );
---
+++ src/components/NewTransactionModal/styles.ts
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+export const NewTransactionModalStyles = styled.div``;

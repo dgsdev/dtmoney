25e159c25ce69fabcbf38d6c47006e4d5d1b966b
Douglas Pinheiro <dgspramos@gmail.com>
2021-12-24 15:24:03 -0300

    Miragejs dados do form post

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,8 @@ import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
-import { Container } from "../Summary/styles";
+import { api } from "../../services/api";
+
 
 import { NewTransactionModalStyles, TransactionTypeContainer, RadioBox } from "./styles";
 interface NewTransactionModalProps {
@@ -20,12 +21,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
     
-    console.log({
+    const data = {
       title,
       value,
       category,
       type,
-    })
+    };
+
+    api.post('/transactions', data);
   }
 
   return (

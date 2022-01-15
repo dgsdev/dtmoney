d2edc2d0598d3ef2035f63c7885c99caa7602310
Douglas Pinheiro <dgspramos@gmail.com>
2022-01-15 11:40:37 -0300

    Inserção de transação com context

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -23,17 +23,22 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();  
     
-    createTransaction(
+    await createTransaction(
       {
         title,
         amount,
         type,
         category,
-      }
-    )
+      })
+    
+    setTitle('');
+    setAmount(0);
+    setType('deposit');
+    setCategory('');
+    onRequestClose();
   }
 
   return (
